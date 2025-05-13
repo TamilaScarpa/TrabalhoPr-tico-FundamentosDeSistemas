b3d0e2917f6087e9f42b7d352d1bfc3e32819f35
TamilaScarpa <tamila.adm@gmail.com>
2025-05-13 12:16:17 -0300

    Delete Trabalho Prático 01 directory

--- Trabalho Prático 01/script.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("botao").addEventListener("click", function () {
-    const mensagem = "Não tenha medo de errar as estrelas nascem do caos!";
-  
-    const p = document.createElement("p");
-    p.textContent = mensagem;
-  
-    this.insertAdjacentElement("afterend", p);
-  });
